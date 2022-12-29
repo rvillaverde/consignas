d4e37320f5590c6d0f817a2d16e7c30a38b5983c
Romina Villaverde <romia.villaverde@gmail.com>
2022-12-29 11:39:16 -0500

    Add loading caption

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -4,11 +4,14 @@ import styles from './loading.module.sass';
 
 const Loading: React.FunctionComponent = () => {
   return (
-    <div className={styles['lds-ellipsis']}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className={styles['loading-wrapper']}>
+      <p>Cargando consignas...</p>
+      <div className={styles['lds-ellipsis']}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
